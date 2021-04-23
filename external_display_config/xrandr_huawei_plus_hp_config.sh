b6b2cc22b451a26f15db70e62af1d44b45afeec2
Demid <demidnau@gmail.com>
2021-04-23 20:51:07 +0300

    change display port

--- external_display_config/xrandr_huawei_plus_hp_config.sh
+++ external_display_config/xrandr_huawei_plus_hp_config.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP1 --primary --mode 3000x2000 --pos 0x0 --rotate normal --output DP1 --mode 1920x1080 --pos 0x2000 --rotate normal --scale 1.5x1.5
+xrandr --output eDP1 --primary --mode 3000x2000 --pos 0x0 --rotate normal --output DP2 --mode 1920x1080 --pos 0x2000 --rotate normal --scale 1.5x1.5
